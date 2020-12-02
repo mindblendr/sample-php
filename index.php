ce1a3dca40500ce7fee7c42983d904be24b96c8e
Aljon Cruz <mindblendr@gmail.com>
2020-12-02 09:47:36 +0800

    Fourth commit

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ require_once 'vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 
-var_dump($faker);
+var_dump($faker); ?>
+<br><br><br>
+
+<?php
+
+echo '<pre>', print_r($_ENV), '</pre><br>';
