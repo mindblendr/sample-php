1080eff6988cd3ad49c1de2fe59cc113a5d8b532
Aljon Cruz <mindblendr@gmail.com>
2021-01-09 15:49:18 +0800

    Check env

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-$faker = Faker\Factory::create();
-echo '<pre>', print_r($faker), '</pre><br>';
-?>
-<br><br><br>
-<h1>TEST</h1>
-<?php
-
-echo '<pre>', print_r($_ENV), '</pre><br>';
+// require_once 'vendor/autoload.php';
+// $faker = Faker\Factory::create();
+echo '<h1>TEST</h1><pre>', print_r($_ENV), '</pre><br>';
